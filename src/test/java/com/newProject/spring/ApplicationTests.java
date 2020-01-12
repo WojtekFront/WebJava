39539e9246b1cc9225257bb84572a1cb0f3d048f
„wojtek <wojciech_lisowski@wp.pl>
2020-01-12 12:56:01 +0100

    init project

---
+++ src/test/java/com/newProject/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newProject.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
